328e64ce79ea3e00e3c86a8f278d3c63398e00c1
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2021-10-01 06:16:42 +0100

    Create main.php
    This is the main PHP file that you should use once you have configured your server and databases correctly.

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+# Name: IP Address to UK Political Constituency 
+# Author: Babatunde Valentine Onabajo
+# Copyright: © 2021 Babatunde Valentine Onabajo (N.B.: That this script is copyrighted does not negate its open source status)
+# Github link: https://github.com/BabatundeOnabajo/IP-Address-To-UK-Political-Constituency
+
+# Description: This is the main PHP file that you should use once you have configured your server and databases correctly. Note that some places here require you to enter your own credentials.
+# Comments will be provided throughout this code to assist you, together with an explanation.
+
+?>
